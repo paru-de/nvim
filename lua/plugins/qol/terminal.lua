df4d1ea1a0ca43a9e3b7b96e70f8ad562f9b22b9
paru-de <patrickr-de@outlook.de>
2024-10-19 18:20:31 +0200

    Add floating terminal toggle

--- lua/plugins/qol/terminal.lua
+++ lua/plugins/qol/terminal.lua
@@ -21,11 +21,30 @@ return {
       direction = 'float',
     }
 
-    function _lazygit_toggle()
+    function LazyGit_toggle()
       lazygit:toggle()
     end
 
+    local floatingterminal = Terminal:new {
+      hidden = true,
+      float_opts = {
+        border = 'single', -- or 'double' or 'shadow' or 'none'
+        width = 120,
+        height = 50,
+        highlights = {
+          border = 'Normal',
+          background = 'Normal',
+        },
+      },
+      direction = 'float',
+    }
+
+    function FloatingTerminal_toggle()
+      floatingterminal:toggle()
+    end
+
     vim.api.nvim_set_keymap('n', '<leader>tt', '<cmd>ToggleTerm<CR>', { noremap = true, silent = true, desc = 'Toggle [T]erminal' })
-    vim.api.nvim_set_keymap('n', '<leader>tg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true, desc = 'Toggle [G]it (LazyGit)' })
+    vim.api.nvim_set_keymap('n', '<leader>tf', '<cmd>lua FloatingTerminal_toggle()<CR>', { noremap = true, silent = true, desc = 'Toggle [F]loating Terminal' })
+    vim.api.nvim_set_keymap('n', '<leader>tg', '<cmd>lua LazyGit_toggle()<CR>', { noremap = true, silent = true, desc = 'Toggle [G]it (LazyGit)' })
   end,
 }
